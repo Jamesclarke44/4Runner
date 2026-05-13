238c8e73cbe822d9a8cb90826bb628efd48551f2
Jamesclarke44 <132843061+Jamesclarke44@users.noreply.github.com>
2026-05-13 06:57:21 -0600

    Update __init__.py

--- workshop/__init__.py
+++ workshop/__init__.py
@@ -1,1 +1,9 @@
+# Import each module as you build it
+# from .brakes import BRAKES
+# from .steering import STEERING
+# etc.
 
+WORKSHOP = {}
+
+# Add modules as you build them:
+# WORKSHOP.update(BRAKES)
